c1cc16724d9e730bd5447cabe5afa819129733f2
Patrick Oscity <patrick.oscity@gmail.com>
2012-08-21 19:37:59 +0200

    Initial Commit

---
+++ BufferedSamples/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BufferedSamples
+//
+//  Created by Patrick Oscity on 21.08.12.
+//  Copyright (c) 2012 Patrick Oscity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
